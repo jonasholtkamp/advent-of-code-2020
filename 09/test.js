c8e3ceca0a14213be9396fb60da456f04a7c6dee
Jonas Holtkamp <jonas.holtkamp@senacor.com>
2020-12-10 11:32:15 +0100

    Fix tests

--- 09/test.js
+++ 09/test.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
 const path = require('path')
 
-const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').trim().split('\n')
+const input = fs.readFileSync(path.join(__dirname, 'test-input.txt'), 'utf8').trim().split('\n')
 
-// it('1', () => {
-//   expect(require('./1')(input, 5)).toEqual(127)
-// })
+it('1', () => {
+  expect(require('./1')(input, 5)).toEqual(127)
+})
 
 it('2', () => {
-  expect(require('./2')(input)).toEqual(62)
+  expect(require('./2')(input, 5)).toEqual(62)
 })
